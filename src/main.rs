48ba4649dbf1af1c5cb579dd44d328eb885c9b67
ilyaselb <il.elboutaybi@com>
2026-01-28 16:11:56 +0100

    Fetch all available pages

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,71 @@ use anyhow::Result;
 use scraper::{Html, Selector};
 use std::collections::HashSet;
 
+async fn fetch_html(client: &reqwest::Client, url: &str) -> Result<String> {
+    let html = client.get(url).send().await?.text().await?;
+
+    Ok(html)
+}
+
+fn parse_listing_page(html: &str) -> Result<(HashSet<String>, Option<String>)> {
+    let document = Html::parse_document(html);
+    let links_selector =
+        Selector::parse("a").map_err(|e| anyhow::anyhow!("bad selector 'a': {e:?}"))?;
+
+    let links: HashSet<String> = document
+        .select(&links_selector)
+        .filter_map(|n| {
+            n.value()
+                .attr("href")
+                .map(|s| s.to_string())
+                .filter(|href| href.contains("/product/"))
+        })
+        .collect();
+
+    let next_sel = Selector::parse("a.next.page-numbers")
+        .map_err(|e| anyhow::anyhow!("bad selector a.next.page-numbers: {e:?}"))?;
+
+    let next_page = document
+        .select(&next_sel)
+        .next()
+        .and_then(|el| el.value().attr("href"))
+        .map(|href| href.to_string());
+
+    Ok((links, next_page))
+}
+
+async fn crawl_shop(client: &reqwest::Client, start_url: &str) -> Result<HashSet<String>> {
+    let mut visited_pages = HashSet::new();
+    let mut all_products = HashSet::new();
+    let mut current = Some(start_url.to_string());
+
+    while let Some(ref page_url) = current {
+        if !visited_pages.insert(page_url.clone()) {
+            break;
+        }
+
+        let html = fetch_html(client, &page_url).await?;
+        let (links, next) = parse_listing_page(&html)?;
+
+        all_products.extend(links);
+
+        println!("Visited: {page_url}");
+        println!("Total unique products so far: {}", all_products.len());
+        println!("Next: {}", next.as_deref().unwrap_or("None"));
+
+        current = next;
+    }
+
+    Ok(all_products)
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let url = "https://www.scrapingcourse.com/ecommerce/";
 
     let client = reqwest::Client::builder()
         .user_agent("ecommerce_scraper/0.1 (test)")
-        .timeout(std::time::Duration::from_secs(15))
+        .timeout(std::time::Duration::from_secs(10))
         .build()?;
 
     let html: String = client.get(url).send().await?.text().await?;
@@ -24,38 +82,7 @@ async fn main() -> Result<()> {
         println!("Not title tag found!");
     }
 
-    let links_selector = Selector::parse("a").unwrap();
-    let links: HashSet<String> = document
-        .select(&links_selector)
-        .filter_map(|n| {
-            n.value()
-                .attr("href")
-                .map(|s| s.to_string())
-                .filter(|href| href.contains("/product/"))
-        })
-        .collect();
-
-    println!("Total links found: {}", links.len());
-
-    // for (i, item) in links.iter().enumerate() {
-    //     println!("Link {}, {}", i + 1, item);
-    //     if i > 3 {
-    //         break;
-    //     }
-    // }
-
-    let next_sel = Selector::parse("a.next.page-numbers").unwrap();
-
-    let next_page = document
-        .select(&next_sel)
-        .next()
-        .and_then(|el| el.value().attr("href"))
-        .map(|href| href.to_string());
-
-    match next_page {
-        Some(url) => println!("Next page: {url}"),
-        None => println!("No next page found on this page"),
-    }
+    crawl_shop(&client, url).await?;
 
     Ok(())
 }
